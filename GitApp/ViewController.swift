f8f41ae447b4ad181f2f09eca2604acc02b1a609
kseniaSova <ksensova@gmail.com>
2018-04-08 17:15:38 +0300

    Initial Commit

---
+++ GitApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitApp
+//
+//  Created by Ксения Червяцова on 08.04.2018.
+//  Copyright © 2018 Ксения Червяцова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
